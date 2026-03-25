bee9bf46185c5571bd24515ab5508c70cefdcb2c
Sifo639 <siefeldinmohame640@gmail.com>
2026-03-25 23:29:35 +0200

    first commit

---
+++ bin/dart_application_1.dart
@@ -0,0 +1,11 @@
+import 'dart:math';
+void main(){
+
+Map fruits = {"prodcut" : "Apple", "price" : 50.5 , "quantity" : 3};
+
+double result = fruits["price"] * fruits["quantity"];
+print(result);
+
+
+}
+
---
+++ bin/se2.dart
@@ -0,0 +1,24 @@
+import 'dart:ffi';
+
+void main() {
+
+for(int i = 0; i <=20; i++){
+if (i % 2 == 0) {
+
+print(i);
+
+}
+
+
+
+}
+
+
+
+
+
+
+
+
+
+}
